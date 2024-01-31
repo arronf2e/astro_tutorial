4f350963a82ecab83bb9ec9d6d9537ac9f010835
arronf2e <arronf2e@163.com>
2024-01-31 21:29:15 +0800

    feat: add react component client:load

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shimmering-croquembouche-a2552e.netlify.app"
+  site: "https://shimmering-croquembouche-a2552e.netlify.app",
+  integrations: [react()]
 });
