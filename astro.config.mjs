29439aa19b264b03fe7f28a37fc3766dd5b031ef
arronf2e <arronf2e@163.com>
2024-01-31 20:51:50 +0800

    feat: add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://shimmering-croquembouche-a2552e.netlify.app"
+});
